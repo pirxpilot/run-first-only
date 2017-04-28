85ea5fa26a2469ac65ad648cf311a7f037f6ed92
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-27 17:52:39 -0700

    add docs and tests

--- Readme.md
+++ Readme.md
@@ -4,7 +4,11 @@
 
 # run-first-only
 
-Execute only first call of the async function.
+Execute only first call of the async function. Like [async.memoize], but limited to no-args
+functions, and without any dependencies. Use it in the browser.
+
+After first call, all subsequent once will get the same result. If first call is still in
+progress, subsequent calls are placed in the queue and notified when initial operation is completed.
 
 ## Install
 
@@ -17,12 +21,28 @@ $ npm install --save run-first-only
 ```js
 var runFirstOnly = require('run-first-only');
 
-runFirstOnly('Rainbow');
+function openDatabase(fn) {
+  // do something async to open DB
+  asyncOperationThatOpensDatabase(connection, name, options, fn)
+}
+
+var open = runFirstOnly(openDatabase);
+
+
+open(function(err, database) {
+  // database is open here
+});
+open(function(err, database) {
+  // it's still open here and it was opened only once
+});
+
 ```
 
 ## License
 
 MIT © [Damian Krzeminski](https://furkot.com)
+
+[async.memoize]: http://caolan.github.io/async/docs.html#memoize
 
 [npm-image]: https://img.shields.io/npm/v/run-first-only.svg
 [npm-url]: https://npmjs.org/package/run-first-only
--- test/run-first-only.js
+++ test/run-first-only.js
@@ -1,9 +1,51 @@
 var should = require('should');
 var runFirstOnly = require('../');
 
-describe('run-first-only node module', function () {
-  it('must have at least one test', function () {
-    runFirstOnly();
-    should.fail('Need to write tests.');
+describe('run-first-only', function () {
+  it('should only call callback once when serial', function (done) {
+    var v = 0;
+    var doThis = runFirstOnly(function(fn) {
+      setTimeout(function() {
+        fn(null, ++v);
+      }, 5);
+    });
+
+    doThis(function(err, value) {
+      should.not.exist(err);
+      value.should.eql(1);
+
+      doThis(function(err, value) {
+        should.not.exist(err);
+        value.should.eql(1);
+
+        done();
+      });
+    });
   });
+
+  it('should only call callback once when parallel', function (done) {
+    var ITER = 5;
+    var v = 0;
+    var called = 0;
+
+    var doThis = runFirstOnly(function(fn) {
+      setTimeout(function() {
+        fn(null, ++v);
+      }, 5);
+    });
+
+    function callback(err, value) {
+      should.not.exist(err);
+      value.should.eql(1);
+
+      if (++called === ITER) {
+        done();
+      }
+    }
+
+    for(var i = 0; i < ITER; i++) {
+      doThis(callback);
+    }
+  });
+
 });
